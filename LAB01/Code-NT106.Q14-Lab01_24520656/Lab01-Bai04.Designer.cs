efe6619d5f1d390e2505261a414c60c70bfc5980
DHuyyy <24520656@gm.uit.edu.vn>
2025-10-08 16:01:27 +0700

    init: bai4

--- LAB01/Code-NT106.Q14-Lab01_24520656/Lab01-Bai04.Designer.cs
+++ LAB01/Code-NT106.Q14-Lab01_24520656/Lab01-Bai04.Designer.cs
@@ -28,12 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labeltieude = new System.Windows.Forms.Label();
+            this.labelChao = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboPhim = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboPhong = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.clbGhe = new System.Windows.Forms.CheckedListBox();
+            this.btnDatVe = new System.Windows.Forms.Button();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labeltieude
+            // 
+            this.labeltieude.AutoSize = true;
+            this.labeltieude.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.labeltieude.Location = new System.Drawing.Point(101, 30);
+            this.labeltieude.Name = "labeltieude";
+            this.labeltieude.Size = new System.Drawing.Size(334, 31);
+            this.labeltieude.TabIndex = 0;
+            this.labeltieude.Text = "Ứng dụng đặt vé xem phim";
+            // 
+            // labelChao
+            // 
+            this.labelChao.AutoSize = true;
+            this.labelChao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChao.Location = new System.Drawing.Point(33, 93);
+            this.labelChao.Name = "labelChao";
+            this.labelChao.Size = new System.Drawing.Size(427, 25);
+            this.labelChao.TabIndex = 1;
+            this.labelChao.Text = "Mời bạn nhập thông tin và chọn phim muốn xem";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(38, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ và tên:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtHoTen.Location = new System.Drawing.Point(135, 162);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(267, 30);
+            this.txtHoTen.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(38, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phim:";
+            // 
+            // cboPhim
+            // 
+            this.cboPhim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPhim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cboPhim.FormattingEnabled = true;
+            this.cboPhim.Location = new System.Drawing.Point(135, 194);
+            this.cboPhim.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboPhim.Name = "cboPhim";
+            this.cboPhim.Size = new System.Drawing.Size(267, 33);
+            this.cboPhim.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(38, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phòng:";
+            // 
+            // cboPhong
+            // 
+            this.cboPhong.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cboPhong.FormattingEnabled = true;
+            this.cboPhong.Location = new System.Drawing.Point(135, 226);
+            this.cboPhong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboPhong.Name = "cboPhong";
+            this.cboPhong.Size = new System.Drawing.Size(108, 33);
+            this.cboPhong.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(38, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ghế:";
+            // 
+            // clbGhe
+            // 
+            this.clbGhe.CheckOnClick = true;
+            this.clbGhe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.clbGhe.FormattingEnabled = true;
+            this.clbGhe.Location = new System.Drawing.Point(135, 260);
+            this.clbGhe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.clbGhe.Name = "clbGhe";
+            this.clbGhe.Size = new System.Drawing.Size(267, 104);
+            this.clbGhe.TabIndex = 7;
+            // 
+            // btnDatVe
+            // 
+            this.btnDatVe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnDatVe.Location = new System.Drawing.Point(135, 388);
+            this.btnDatVe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDatVe.Name = "btnDatVe";
+            this.btnDatVe.Size = new System.Drawing.Size(89, 36);
+            this.btnDatVe.TabIndex = 8;
+            this.btnDatVe.Text = "Đặt vé";
+            this.btnDatVe.UseVisualStyleBackColor = true;
+            this.btnDatVe.Click += new System.EventHandler(this.btnDatVe_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtKetQua.Location = new System.Drawing.Point(38, 428);
+            this.txtKetQua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtKetQua.Multiline = true;
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtKetQua.Size = new System.Drawing.Size(365, 121);
+            this.txtKetQua.TabIndex = 9;
+            // 
+            // Lab01_Bai04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Lab01_Bai04";
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(510, 577);
+            this.Controls.Add(this.labelChao);
+            this.Controls.Add(this.labeltieude);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.btnDatVe);
+            this.Controls.Add(this.clbGhe);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cboPhong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboPhim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Lab01_Bai04";
+            this.Text = "Bài 04 - Quản lý phòng vé";
+            this.Load += new System.EventHandler(this.Lab01_Bai04_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labeltieude;
+        private System.Windows.Forms.Label labelChao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboPhim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboPhong;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox clbGhe;
+        private System.Windows.Forms.Button btnDatVe;
+        private System.Windows.Forms.TextBox txtKetQua;
     }
 }
--- LAB01/Code-NT106.Q14-Lab01_24520656/Lab01-Bai04.cs
+++ LAB01/Code-NT106.Q14-Lab01_24520656/Lab01-Bai04.cs
@@ -12,9 +12,100 @@ namespace Code_NT106.Q14_Lab01_24520656
 {
     public partial class Lab01_Bai04 : Form
     {
+        // Dictionary lưu giá phim
+        Dictionary<string, int> giaPhim = new Dictionary<string, int>()
+    {
+        { "Đào, phở và piano", 45000 },
+        { "Mai", 100000 },
+        { "Gặp lại chị bầu", 70000 },
+        { "Tarot", 90000 }
+    };
+
+        // HashSet lưu ghế đã bán
+        HashSet<string> gheDaMua = new HashSet<string>();
+
         public Lab01_Bai04()
         {
             InitializeComponent();
         }
+
+        private void Lab01_Bai04_Load(object sender, EventArgs e)
+        {
+            // Thêm phim vào combobox
+            cboPhim.Items.AddRange(giaPhim.Keys.ToArray());
+
+            // Thêm phòng
+            cboPhong.Items.AddRange(new object[] { "1", "2", "3" });
+
+            // Thêm danh sách ghế
+            for (char hang = 'A'; hang <= 'C'; hang++)
+            {
+                for (int so = 1; so <= 5; so++)
+                {
+                    clbGhe.Items.Add($"{hang}{so}");
+                }
+            }
+        }
+
+        private void btnDatVe_Click(object sender, EventArgs e)
+        {
+            string ten = txtHoTen.Text.Trim();
+            string phim = cboPhim.SelectedItem?.ToString();
+            string phong = cboPhong.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(ten) || phim == null || phong == null)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                return;
+            }
+
+            if (clbGhe.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn ít nhất 1 ghế!");
+                return;
+            }
+
+            int giaChuan = giaPhim[phim];
+            double tongTien = 0;
+            List<string> gheChon = new List<string>();
+
+            foreach (var item in clbGhe.CheckedItems)
+            {
+                string ghe = item.ToString();
+
+                // Check ghế đã mua chưa
+                if (gheDaMua.Contains($"{phong}-{ghe}"))
+                {
+                    MessageBox.Show($"Ghế {ghe} ở phòng {phong} đã được mua!");
+                    continue;
+                }
+
+                // Xác định loại ghế
+                double giaVe = 0;
+                if (ghe == "A1" || ghe == "A5" || ghe == "C1" || ghe == "C5")
+                    giaVe = giaChuan * 0.25;
+                else if (ghe.StartsWith("B") && ghe != "B1" && ghe != "B5")
+                    giaVe = giaChuan * 2;
+                else
+                    giaVe = giaChuan;
+
+                tongTien += giaVe;
+                gheChon.Add(ghe);
+
+                // Đánh dấu đã mua
+                gheDaMua.Add($"{phong}-{ghe}");
+            }
+
+            // Xuất kết quả
+            txtKetQua.Text =
+                $"Khách hàng: {ten}\r\n" +
+                $"Phim: {phim}\r\n" +
+                $"Phòng: {phong}\r\n" +
+                $"Ghế: {string.Join(", ", gheChon)}\r\n" +
+                $"Tổng tiền: {tongTien:N0} VNĐ";
+        }
+
+        
     }
+
 }
